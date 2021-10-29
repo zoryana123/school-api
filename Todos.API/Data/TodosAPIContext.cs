2ca3c8e696ed373cf6cf26797dff40a6c376c6da
Zoryana <zkua91@gmail.com>
2021-10-28 20:07:16 -0400

    Add project files.

---
+++ Todos.API/Data/TodosAPIContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Todos.API.Models;
+
+    public class TodosAPIContext : DbContext
+    {
+        public TodosAPIContext (DbContextOptions<TodosAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Todos.API.Models.Todo> Todo { get; set; }
+    }
